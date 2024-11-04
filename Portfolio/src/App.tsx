4bb9806e6893a37ddbeaf0f7451d66617b3e588a
Muhammad Suleman <145265998+SulemanWaraich@users.noreply.github.com>
2024-11-04 21:20:05 +0500

    update portfolio

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -1,23 +1,51 @@
-import './App.css'
-import About from './components/About'
-import Home from './components/Home'
-import Skill from './components/Skill'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
+import './App.css';
+import React, { useRef } from 'react';
+import Home from "./components/Home.tsx";
+import About from "./components/About.tsx";
+import Skill from "./components/Skill.tsx";
+import Projects from "./components/Projects.tsx";
+import Contact from "./components/Contact.tsx";
 
 function App() {
+  const homeRef = useRef<HTMLDivElement>(null);
+  const aboutRef = useRef<HTMLDivElement>(null);
+  const skillRef = useRef<HTMLDivElement>(null);
+  const projectsRef = useRef<HTMLDivElement>(null);
+  const contactRef = useRef<HTMLDivElement>(null);
+
+  const scrollToRef = (ref: React.RefObject<HTMLDivElement>) => {
+    if (ref.current) {
+      ref.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
   return (
-   <>
-    <div className='flex flex-col items-center scroll-smooth'>
-    <Home></Home>
-    <About></About>
-    <Skill></Skill>
-    <Projects></Projects>
-    <Contact></Contact>
-    </div>
-   </>
-  )
+    <>
+      <div className='flex flex-col items-center scroll-smooth font-body'>
+        <div ref={homeRef}>
+        <Home 
+          onScrollToHome={() => scrollToRef(homeRef)}
+          onScrollToAbout={() => scrollToRef(aboutRef)}
+          onScrollToSkill={() => scrollToRef(skillRef)}
+          onScrollToProjects={() => scrollToRef(projectsRef)}
+          onScrollToContact={() => scrollToRef(contactRef)}
+        />
+        </div>
+        <div ref={aboutRef}>
+          <About />
+        </div>
+        <div ref={skillRef}>
+          <Skill />
+        </div>
+        <div ref={projectsRef}>
+          <Projects />
+        </div>
+        <div ref={contactRef}>
+          <Contact />
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
